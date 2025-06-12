3f1453bd4dceba0b81928571c4f05100ac331f22
HalmondD <duonganhkhoa2002@gmail.com>
2025-06-12 09:38:30 +0700

    update bin

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "app.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
